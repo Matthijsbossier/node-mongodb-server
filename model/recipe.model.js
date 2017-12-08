c7a1998d933759419472d48fe37637c75d4fd37f
Jordy Huijgens <jhuijgens1@live.nl>
2017-12-08 20:01:28 +0100

    update a

--- model/recipe.model.js
+++ model/recipe.model.js
@@ -1,38 +1,41 @@
-// Setup.
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ingredientSchema = require('../model/ingredient.model.js');
 
-// Ingredient voor in het recept.
-const Ingredient = require('../model/ingredient.model');
-
-// Recept schema.
+// definitie van je database:
 const RecipeSchema = new Schema({
-    name: String,
+    //_id: String,t
+    name: {
+        type:String,
+        required: true,
+        unique: true
+    },
     description: String,
     imagePath: String,
-    ingredients: 
-    [{
-        // Deze lijst gaat dus ingredient objecten bevatten.
-        ingredient: Ingredient
-    }]
-}, 
-
-{
-    timestamps: true
+    ingredients: [ingredientSchema]
 });
 
-// Het recept.
+
 const Recipe = mongoose.model('recipe', RecipeSchema);
 
-// Een voorbeeld recept aanmaken, bevat (nog) geen ingredienten.
-const recipe = new Recipe({
-    name: 'Pizza salami',
-    description: 'Test recipe',
-    imagePath: 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
-    ingredients: 
-    [
-        
-    ] 
-}).save();
-
+// alles wat naar buiten toe wordt geexporteerd:
 module.exports = Recipe;
+//ik zoek hier naareen recipe met de naam allIngredients als deze nog niet bestaat kan je deze aanmaken hieraan voegen we alle ingredients toe
+Recipe.find({name : "AllIngredients"}, function (err, docs) {
+        if (docs.length){
+            //doe niks
+        }
+        else{
+          const recipe = new Recipe({
+              name: 'AllIngredients',
+              description: 'Test recipe',
+              imagePath: 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+              ingredients: [
+                  { name: 'Pizzabodem', amount: 1},
+                  { name: 'Kaas', amount: 1},
+                  { name: 'Tomatensaus', amount: 1},
+                  { name: 'Salami', amount: 5},
+                  ]
+          }).save();
+        }
+    });
