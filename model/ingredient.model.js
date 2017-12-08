3dd9e9ab75d43eb47d5f4a2a931a1d6899f1d265
Jordy Huijgens <jhuijgens1@live.nl>
2017-12-08 20:02:04 +0100

    update b

---
+++ model/ingredient.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// definitie van je database:
+const IngedientSchema = new Schema({
+    //_id: String,
+    name: String,
+    amount: Number
+});
+
+module.exports = IngedientSchema;
+//t
